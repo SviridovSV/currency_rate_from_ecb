5052704fea708f7c907fd0dcc9e72e364a91c361
SviridovSV <sviridov.sv82@gmail.com>
2017-02-04 01:30:35 +0200

    fix activerecord gem

--- lib/currency_rate_from_ecb/config/data_connection.rb
+++ lib/currency_rate_from_ecb/config/data_connection.rb
@@ -2,7 +2,7 @@ require 'yaml'
 require 'active_record'
 module DataConnection
 
-  dbconfig = YAML.load_file('config/database.yml')
+  dbconfig = YAML.load_file('database.yml')
 
   ActiveRecord::Base.establish_connection(dbconfig)
 end
