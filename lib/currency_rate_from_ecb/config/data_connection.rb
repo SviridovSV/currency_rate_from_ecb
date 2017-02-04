9e999c66c808f04b96eb95cf0272295961b72823
SviridovSV <sviridov.sv82@gmail.com>
2017-02-05 01:05:20 +0200

    delete yml, make connection straight

--- lib/currency_rate_from_ecb/config/data_connection.rb
+++ lib/currency_rate_from_ecb/config/data_connection.rb
@@ -3,6 +3,6 @@ module DataConnection
 
   ActiveRecord::Base.establish_connection(
     adapter: "sqlite3",
-    database: "db/database.sqlite3"
+    database: "currency_rate_from_ecb/db/database.sqlite3"
   )
 end
